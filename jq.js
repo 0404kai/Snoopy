a3888a62ac6266264839a77920daf212c7ffd06e
0404kai <bthd0404@gmail.com>
2021-08-07 01:24:34 +0800

    Update jq.js

--- jq.js
+++ jq.js
@@ -3,10 +3,11 @@ $(document).ready(function() {
         $('.box').toggle('slow');
         $('.b1').hide();
         $('.b2').toggle();
-        $('.b2').click(function() {
-            $('.box').toggle('slow');
-            $('.b2').hide();
-            $('.b1').toggle();
-        });
+        $('.bubble1,.bubble2').css('opacity',1);
+    });
+    $('.b2').click(function() {
+        $('.box').toggle('slow');
+        $('.b2').hide();
+        $('.b1').toggle();
     });
 });
